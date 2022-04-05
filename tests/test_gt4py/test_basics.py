385b206e8287f9db05d288a1950e5e8e9a2bfefe
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-04-05 22:52:00 +0200

    add test with basic interpolation to surface (nearest-neighbor)

--- tests/test_gt4py/test_basics.py
+++ tests/test_gt4py/test_basics.py
@@ -29,7 +29,7 @@ def test_copy() -> None:
             out_fld = in_fld[0, 0, 0]
 
     # Define fields
-    (nx, ny, nz) = shape3d = (5, 5, 5)
+    (nx, ny, nz) = shape3d = (4, 5, 6)
     in_data = np.zeros(shape3d, DTYPE)
     for i in range(nx):
         for k in range(nz):
@@ -72,7 +72,7 @@ def test_vertical_shift_periodic() -> None:
             out_fld = in_fld[0, 0, -1]
 
     # Define fields
-    (nx, ny, nz) = shape3d = *shape2d, _ = (5, 5, 5)
+    (nx, ny, nz) = shape3d = *shape2d, _ = (4, 5, 6)
     in_data = np.zeros(shape3d, DTYPE)
     for i in range(nx):
         for k in range(nz):
@@ -100,3 +100,61 @@ def test_vertical_shift_periodic() -> None:
 
     # Now the field validates against the reference
     assert np.equal(in_store, ref).all()
+
+
+def test_vert_intp_full() -> None:
+    """Interpolate a 3D field in the vertical to a given surface.
+
+    The surface value lies on the grid, making nearest neighbor interpolation
+    sufficient.
+
+    """
+
+    @gts.stencil(backend=BACKEND)
+    def interpolate(
+        in_fld: gts.Field[gts.IJK, DTYPE],
+        grid_fld: gts.Field[gts.IJK, DTYPE],
+        val: float,
+        intp_fld: gts.Field[gts.IJ, DTYPE],
+        wk1_fld: gts.Field[gts.IJ, DTYPE],
+    ) -> None:
+        """Shift field upward by one, moving the uppermost layer to the bottom."""
+        with computation(FORWARD), interval(-1, None):
+            intp_fld = in_fld[0, 0, 0]
+        with computation(FORWARD), interval(...):
+            if grid_fld == val:
+                intp_fld = in_fld[0, 0, 0]
+
+    # Define fields
+    (nx, ny, nz) = shape3d = *shape2d, _ = (4, 5, 6)
+    grid_data = np.zeros(shape3d, DTYPE)
+    in_data = np.zeros(shape3d, DTYPE)
+    for k in range(nz):
+        in_data[:, :, k] = k
+        for i in range(nx):
+            grid_data[i, :, k] = i + k
+    val = 6.0
+
+    # Define stores
+    in_store = gt4py.storage.from_array(
+        in_data, backend=BACKEND, default_origin=ORIGIN, dtype=DTYPE
+    )
+    grid_store = gt4py.storage.from_array(
+        grid_data, backend=BACKEND, default_origin=ORIGIN, dtype=DTYPE
+    )
+    intp_store = gt4py.storage.empty(
+        shape=(nx, ny), mask="IJ", backend=BACKEND, default_origin=(0, 0), dtype=DTYPE
+    )
+    wk1_store = gt4py.storage.empty(
+        shape=(nx, ny), mask="IJ", backend=BACKEND, default_origin=(0, 0), dtype=DTYPE
+    )
+
+    # Create test reference and ensure the output array doesn't validate yet
+    ref = np.take(in_data, np.where(grid_data < val, 1, 0).sum(axis=2).clip(max=nz - 1))
+    assert not np.equal(intp_store, ref).all()
+
+    # Run test: Interpolate field to surface
+    interpolate(in_store, grid_store, val, intp_store, wk1_store)
+
+    # Now the output array validates
+    assert np.equal(intp_store, ref).all()
