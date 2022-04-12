6a1e77992986fbacae16472bd43750992205c913
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-04-12 10:33:48 +0200

    refine setup.py

--- minimal_build_examples/skbuild/setup.py
+++ minimal_build_examples/skbuild/setup.py
@@ -2,18 +2,28 @@
 from __future__ import annotations
 
 # Standard library
-import os
 import sys
+from pathlib import Path
 
 # Third-party
 from setuptools import find_packages
 from skbuild import setup
 
+# Python version requirement
 python_requires: str = ">=3.9"
+
+# Runtime dependencies (top-level and unpinned)
 install_requires: list[str] = []
 
-# Obtain currently active Python version for cmake
-current_python: str = f"{sys.version_info.major}.{sys.version_info.minor}"
+# Obtain version and root of currently active Python environment for cmake
+curr_python_version: str = f"{sys.version_info.major}.{sys.version_info.minor}"
+curr_python_root: str = str(Path(sys.executable).parent.parent)  # remove `bin/python`
+
+# Arguments passed to cmake by scikit-build
+cmake_args: list[str] = [
+    f"-DCMAKE_PREFIX_PATH={curr_python_root}",
+    f"-DCMAKE_PYTHON_VERSION={curr_python_version}",
+]
 
 setup(
     name="hello",
@@ -21,8 +31,5 @@ setup(
     package_dir={"": "src"},
     python_requires=python_requires,
     install_requires=install_requires,
-    cmake_args=[
-        f"-DCMAKE_PREFIX_PATH={os.environ['CONDA_PREFIX']}",
-        f"-DCMAKE_PYTHON_VERSION={current_python}",
-    ],
+    cmake_args=cmake_args,
 )
